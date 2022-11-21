373efddaab41ed5d4e3289918c36f2246aec8ac7
dominictrent <Dominctrent@icloud.com>
2022-11-21 10:25:27 -0700

    i

---
+++ main.js
@@ -0,0 +1,18 @@
+// if statemernt
+// input
+//alert
+
+let msg = "Username is valid";
+let message = "username is invalid";
+
+document.getElementById("Username");
+document.getElementById("Password");
+document.addEventListener;
+
+if (Username === "Username") {
+  Username = "valid";
+  alert(msg);
+} else if (Username === "username") {
+  username = "invalid";
+  alert(message);
+}
